594b401550556693f6751f5617cb513b47d8a0b3
Bsindhujareddy <sindhuja49b@gmail.com>
2026-04-14 12:32:24 +0530

    fc

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api";
+const API = "http://nikbsoftmsapi.digidog.in/:5000/api";
 
 export const getMenu = () => axios.get(`${API}/menu`);
 export const placeOrder = (data) => axios.post(`${API}/orders`, data);
